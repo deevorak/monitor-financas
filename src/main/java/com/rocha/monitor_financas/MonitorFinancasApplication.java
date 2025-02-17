49160a4fcae3fc010fe52b42a37c7458119149dc
Carlos Rocha <carlosesr.inf@gmail.com>
2025-02-16 22:02:21 -0300

    transformando em projeto springboot

---
+++ src/main/java/com/rocha/monitor_financas/MonitorFinancasApplication.java
@@ -0,0 +1,13 @@
+package com.rocha.monitor_financas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonitorFinancasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonitorFinancasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rocha/monitor_financas/MonitorFinancasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rocha.monitor_financas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonitorFinancasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
